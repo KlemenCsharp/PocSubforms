07344cfa7eb3c8447f22f58aaabcf9122dde4c3e
Klemen Zupan <zupanklem@gmail.com>
2021-03-09 07:55:36 +0100

    Created parent component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentFormComponent } from './parent-form/parent-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentFormComponent
   ],
   imports: [
     BrowserModule,
